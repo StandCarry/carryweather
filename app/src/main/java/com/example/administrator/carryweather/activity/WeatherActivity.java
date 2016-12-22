c55d339c7e3923356f4ca61eab4ab5ce92dec94e
standcarry <standcarry@163.com>
2016-12-22 17:12:58 +0800

    完成后台自动更新天气

--- app/src/main/java/com/example/administrator/carryweather/activity/WeatherActivity.java
+++ app/src/main/java/com/example/administrator/carryweather/activity/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.carryweather.activity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -23,6 +24,7 @@ import com.bumptech.glide.Glide;
 import com.example.administrator.carryweather.R;
 import com.example.administrator.carryweather.gson.Forecast;
 import com.example.administrator.carryweather.gson.Weather;
+import com.example.administrator.carryweather.service.AutoUpdateService;
 import com.example.administrator.carryweather.util.HttpUtil;
 import com.example.administrator.carryweather.util.Utility;
 
@@ -167,6 +169,8 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather",responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+                            Intent intent=new Intent(WeatherActivity.this, AutoUpdateService.class);
+                            startService(intent);
 
                         }else {
                             Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
